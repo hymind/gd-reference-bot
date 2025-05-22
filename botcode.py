15cc8dc0be7b8f2d423049ae251ce01bcd89cf66
hymind <shanibaram@gmail.com>
2025-05-22 18:40:04 +0300

    added functionality

--- botcode.py
+++ botcode.py
@@ -8,13 +8,10 @@ client = discord.Client(intents=intents)
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
-
 @client.event
 async def on_message(message):
-    if message.author == client.user:
-        return
+    for name in levelnames:
+        if name in str(message.content).lower() and len(message.content) < len(name)*5:
+            await message.reply(levelnames[name], mention_author=True)
 
-    if message.content.startswith('does this shit work'):
-        await message.channel.send('yes')
-
-client.run('MTI2MDIwNjY2ODQwMzE3OTUzMg.G5M72N.vP7gEdLhOgKn3Rm1bIfangEa_qLLKSDMRDt7ac')
+client.run('bot_token_here')
